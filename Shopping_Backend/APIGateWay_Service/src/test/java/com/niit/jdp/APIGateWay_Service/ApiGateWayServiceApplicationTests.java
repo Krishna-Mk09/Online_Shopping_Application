e24e35e626d1be77a3d5fc2298f7a84c33cec85c
M V K <krishna.mugala9@gmail.com>
2023-09-18 09:21:44 +0530

    created project

---
+++ Shopping_Backend/APIGateWay_Service/src/test/java/com/niit/jdp/APIGateWay_Service/ApiGateWayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niit.jdp.APIGateWay_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGateWayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
